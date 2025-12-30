e262eb73231917ad6b5bb683058f3c174e134067
MasayaEzure <78490228+MasayaEzure@users.noreply.github.com>
2025-12-30 17:25:50 +0900

    chore: bootstrap Spring Boot project and local DB setup

---
+++ src/main/java/com/example/approval_workflow_api/ApprovalWorkflowApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.approval_workflow_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprovalWorkflowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApprovalWorkflowApiApplication.class, args);
+	}
+
+}
