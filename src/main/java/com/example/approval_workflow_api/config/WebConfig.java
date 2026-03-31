31ae6b76020c23c74df452367ab69f56bbbc82cd
MasayaEzure <78490228+MasayaEzure@users.noreply.github.com>
2026-04-01 08:21:43 +0900

    feat: 引数リゾルバの実装が完了

--- src/main/java/com/example/approval_workflow_api/config/WebConfig.java
+++ src/main/java/com/example/approval_workflow_api/config/WebConfig.java
@@ -5,12 +5,26 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.example.approval_workflow_api.interceptor.PerformanceInterceptor;
+import com.example.approval_workflow_api.resolver.CurrentUserResolver;
+import java.util.List;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    private final CurrentUserResolver currentUserResolver;
+
+    public WebConfig(CurrentUserResolver currentUserResolver) {
+        this.currentUserResolver = currentUserResolver;
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new PerformanceInterceptor()).addPathPatterns("/api/**");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(currentUserResolver);
     }
 }
